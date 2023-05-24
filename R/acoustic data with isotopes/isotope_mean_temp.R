b0b7966c700c6ce0938ddbd65f90ec929af9f891
benjamin.hlina <benjamin.hlina@gmail.com>
2023-05-24 16:05:36 -0400

    Add in linear models, assess fits, and outliers. Create model fit dataframe and main effects as rds that we will merge into one large rds to save as an excel file

--- R/acoustic data with isotopes/isotope_mean_temp.R
+++ R/acoustic data with isotopes/isotope_mean_temp.R
@@ -219,77 +219,129 @@ glance_summary <- map_df(glance_list, ~as.data.frame(.x), .id = "id") %>%
 
 glance_summary
 
-glance_summary <- glance_summary %>% 
+gs_d13c <- glance_summary %>% 
   mutate(
     delta_AIC = AIC - first(AIC), 
     AIC_weight = exp(-0.5 * delta_AIC) / sum(exp(-0.5 * delta_AIC))
   ) %>% 
   dplyr::select(model:AIC, delta_AIC, AIC_weight, BIC:df.residual)
-glance_summary
-
-glance_summary %>%
-  openxlsx::write.xlsx(here::here("results",
-                                  "d13C Habitat",
-                                  "d13C_temp_model_selection.xlsx"))
+gs_d13c
 
 # ---- create specific stuff for model saving -----
-car::Anova(m)
-summary(m)
 
-main_effects <- tidy(car::Anova(m))
+drop1(m, .~., test = "F")
+shapiro.test(residuals(m))
+
+par(mfrow = c(2, 2))
+plot(m)
+par(mfrow = c(1, 1))
+
+Anova(mod = m, type = 3)
 
 
 
-ind_effects <- tidy(m)
+me_d13c <- tidy(car::Anova(m,  type = 3))
+me_d13c
 
-
-# main_effects %>% 
-main_effects %>% 
-  openxlsx::write.xlsx(here::here("results",
-                                  "d13C Habitat",
-                                  "d13C_temp_lmer_main_effect.xlsx"))
-ind_effects %>%
-  openxlsx::write.xlsx(here::here("results",
-                                  "d13C Habitat",
-                                  "d13C_temp_lmer_ind_effect.xlsx"))
-
-# ---- Nitrogen vs movement -----
+# ---- Nitrogen vs movement with large fish  -----
 glimpse(ati)
 
-m3 <- glmmTMB(n_15 ~ mean_temp * fish_basin + (1|floy_tag) +
-                # (1|temp_p_sem) + (1| temp_m_sem),
-              data = ati,
-              family = Gamma(link = "identity"),
-              # control = glmmTMBControl(optimizer=optim,
-              #                          optArgs = list(method = "BFGS")),
-              REML = TRUE)
 
-# model fit evaluation 
-res3 <- simulateResiduals(m3)
-plot(res3)
-Anova(m3, type = "II")
+m3 <- lm(n_15 ~ mean_temp * fish_basin, 
+         data = ati,
+         contrasts = list(fish_basin = contr.sum))
+
+Anova(m3, type = 3)
+
 # model section 
-m4 <- update(m3, ~ mean_temp * sem + (1|floy_tag), 
-             # control = glmmTMBControl(optimizer=optim,
-             #                          optArgs = list(method = "BFGS"))
-)
-m5 <- update(m3, ~ mean_temp)
+m4 <- update(m3, ~ mean_temp)
+m5 <- update(m3, ~ fish_basin)
+m6 <- update(m3, ~ mean_temp + fish_basin)
 
 
-model_list <- list(m3, m4, m5)
+
+model_list <- list(m3, m4, m5, m6)
 # give the elements useful names
 names(model_list) <- c("m3", 
-                       "m4", "m5")
+                       "m4", "m5", "m6")
 
-glance(m3)
+glance(m6)
 # get the summaries using `lapply
 
 summary_list <- lapply(model_list, function(x) tidy(x, parametric = TRUE))
 glance_list <- lapply(model_list, glance)
 
+
 glance_summary <- map_df(glance_list, ~as.data.frame(.x), .id = "id") %>% 
   mutate(model = lapply(model_list, formula) %>%
-           as.character() 
+           as.character()
+  ) %>% 
+  dplyr::select(model, id:df.residual) %>% 
+  arrange(AIC)
+
+glance_summary
+
+gs_d15n_outlier <- glance_summary %>% 
+  mutate(
+    delta_AIC = AIC - first(AIC), 
+    AIC_weight = exp(-0.5 * delta_AIC) / sum(exp(-0.5 * delta_AIC))
+  ) %>% 
+  dplyr::select(model:AIC, delta_AIC, AIC_weight, BIC:df.residual)
+gs_d15n
+
+
+# ---- create specific stuff for model saving -----
+car::Anova(m3, type = "III")
+
+par(mfrow = c(2, 2))
+plot(m3)
+par(mfrow = c(1, 1))
+shapiro.test(residuals(m3))
+
+me_d15n_outlier <- tidy(car::Anova(m3, type = "III"))
+cooksD <- cooks.distance(m3)
+influential <- cooksD[(cooksD > (3 * mean(cooksD, na.rm = TRUE)))]
+influential
+
+# the one fish that we tagged that is at a higher trophic level really pulls 
+# the results to accomedate this we will regress a second time without it 
+
+# ---- Nitrogen vs movement without large fish  -----
+glimpse(ati_s)
+
+
+m7 <- lm(n_15 ~ mean_temp * fish_basin, 
+         data = ati_s,
+         contrasts = list(fish_basin = contr.sum),
+)
+
+Anova(m7, type = 3)
+
+# model section 
+m8 <- update(m7, ~ mean_temp)
+m9 <- update(m7, ~ fish_basin)
+m10 <- update(m7, ~ mean_temp + fish_basin)
+
+
+
+model_list <- list(m7, m8, m9, m10)
+# give the elements useful names
+names(model_list) <- c("m7", 
+                       "m8", "m9", "m10")
+
+glance(m7)
+# get the summaries using `lapply
+
+summary_list <- lapply(model_list, function(x) tidy(x, parametric = TRUE))
+glance_list <- lapply(model_list, glance)
+# glance_list <- lapply(glance_list, function(x) mutate_at(x, 
+#                                                          .vars = "logLik", 
+#                                                          as.numeric))
+
+
+glance_summary <- map_df(glance_list, ~as.data.frame(.x), .id = "id") %>% 
+  mutate(model = lapply(model_list, formula) %>%
+           as.character()
   ) %>% 
   dplyr::select(model, id:df.residual) %>% 
   arrange(AIC)
@@ -297,39 +349,155 @@ glance_summary <- map_df(glance_list, ~as.data.frame(.x), .id = "id") %>%
 
 glance_summary
 
-glance_summary <- glance_summary %>% 
+gs_d15n <- glance_summary %>% 
   mutate(
     delta_AIC = AIC - first(AIC), 
     AIC_weight = exp(-0.5 * delta_AIC) / sum(exp(-0.5 * delta_AIC))
   ) %>% 
   dplyr::select(model:AIC, delta_AIC, AIC_weight, BIC:df.residual)
-glance_summary
-
-glance_summary %>%
-  openxlsx::write.xlsx(here::here("results",
-                                  "d15N Habitat",
-                                  "d15N_temp_model_selection.xlsx"))
+gs_d15n
 
 # ---- create specific stuff for model saving -----
-car::Anova(m3)
-# summary(m3)
+car::Anova(m7, type = "III")
+par(mfrow = c(2, 2))
+plot(m7)
+par(mfrow = c(1, 1))
+shapiro.test(residuals(m7))
 
-main_effects <- tidy(car::Anova(m3))
-
-
-
-ind_effects <- tidy(m3)
-summary(m3)
-
-ind_effects
-# main_effects %>% 
-main_effects %>% 
-  openxlsx::write.xlsx(here::here("results",
-                                  "d15N Habitat",
-                                  "d15N_temp_lmer_main_effect.xlsx"))
-ind_effects %>%
-  openxlsx::write.xlsx(here::here("results",
-                                  "d15N Habitat",
-                                  "d15N_temp_lmer_ind_effect.xlsx"))
+me_d15n <- tidy(car::Anova(m7, type = "III"))
 
 
+# ---- combine main effects and model fit together and save as an RDS ----
+# we will combine the rds for all three metrics later in another script 
+
+model_fit <- bind_rows(list(gs_d13c = gs_d13c, 
+                            gs_d15n = gs_d15n,
+                            gs_d15n_outlier = gs_d15n_outlier), 
+                       .id = "id") %>% 
+  mutate(
+    metric = "temp"
+  ) %>% 
+  dplyr::select(id, metric, model, r.squared:df.residual)
+
+
+model_effects <- bind_rows(list(me_d13c = me_d13c, 
+                                me_d15n = me_d15n,
+                                me_d15n_outlier = me_d15n_outlier), 
+                           .id = "id") %>% 
+  mutate(
+    metric = "temp"
+  ) %>% 
+  dplyr::select(id, metric, term, p.value)
+
+model_fit
+
+model_effects
+
+write_rds(model_fit, here("Results", 
+                          "temp_isotope_model_fit.rds"))
+write_rds(model_effects, here("Results", 
+                              "temp_isotope_model_effects.rds"))
+
+# ---- plot mean temp across months for d13C -----
+# descdist(ati$mean_temp)
+# 
+# 
+# m7 <- glm(mean_temp ~ c_13 * n_15, 
+#           data = ati, 
+#           family = Gamma()
+#           )
+# 
+# Anova(m7, type = 3)
+# 
+# res <- simulateResiduals(m7)
+# 
+# plot(res)
+# shapiro.test(residuals.glm(m7))
+# 
+# 
+# 
+# summary(ati$n_15)
+# # 
+# nd <- expand_grid(
+#   c_13 = seq(-25.5, -31.5, length.out = 100),
+#   n_15 = seq(8.75, 12, length.out = 100),
+#   mean_temp = seq(4.25, 7.75, length.out = 100)
+# 
+# )
+# # 
+# fits <- predict(m7, newdata = nd, type = "response")
+# 
+# preds <- bind_cols(nd, fits) %>%
+#   rename(
+#     fit = ...4
+#   )
+# 
+# preds
+# # 
+# # 
+# # 
+# # 
+# # 
+# # 
+# # 
+# ggplot() +
+#  
+#   geom_raster(data = preds, aes(x = c_13,
+#                               y = n_15, fill = fit), ) + 
+#   geom_point(data = ati, size = 4,
+#              aes(y = n_15, x = c_13,
+#                  colour = mean_temp),
+#              # shape = 21, stroke = 0.8
+#              ) +
+# #   # stat_ellipse(aes(colour = fish_basin), 
+# #   #              type = "norm", linetype = 1,
+# #   # linewidth = 1) +
+# #   # geom_errorbar(aes(xmin = mean_temp - sem, 
+# #   #                   xmax = mean_temp + sem), width = 0.05) +
+#   scale_fill_viridis_c(name = "Predicted\nTemperature Use (°C)", 
+#                        option = "D",alpha = 0.5, 
+#                        breaks = seq(4.5, 7.5, 1),
+#                        # limit = c(4, 8)
+#                        ) +
+#   scale_colour_viridis_c(
+#     # begin = 0.25, end = 0.85,
+#                        option = "D",
+#                        name = "Observed\nTemperature Use (°C)", 
+#                        ) +
+#   # scale_y_continuous(breaks = rev(seq(-26, -31, -1))) +
+#   # facet_wrap(.~ fish_basin) +
+#   coord_cartesian(expand = FALSE) +
+#   theme_bw(base_size = 15) +
+#   theme(
+#     legend.title = element_text(hjust = 0.5), 
+#     panel.grid = element_blank(),
+#     # legend.position = c(0.85, 0.9)
+#   ) +
+#   labs(
+#     x = expression(paste(delta ^ 13, "C")),
+#     y = expression(paste(delta ^ 15, "N"))) -> p3
+# 
+# 
+# p3
+# ggsave(filename = here("Plots",
+#                        "temp use and isotopes",
+#                        "mean_temp_month_d13c_raster_pred.png"), plot = p3,
+#        width = 11, height = 8.5)
+# # 
+# 
+# write_rds(p, here("Saved Plots",
+#                   "c_13_temp_overall.rds"))
+# 
+# 
+# ggplot(data = preds, aes(x = mean_temp, y = fit)) + 
+#   geom_(aes(fill = fit), linewidth = 1) + 
+#   scale_fill_viridis_c(option = "D", 
+#                        name = expression(paste(delta ^ 13, "C"))) + 
+#   theme_bw(base_size = 15) + 
+#   facet_wrap(.~ fish_basin) +
+#   theme(
+#     panel.grid = element_blank()
+#   ) + 
+#   labs(
+#     y = expression(paste(delta ^ 13, "C")), 
+#     x = "Mean Monthly Temperature Use (°C)"
