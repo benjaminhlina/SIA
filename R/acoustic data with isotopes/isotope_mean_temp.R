bb5375a43f4a9ce6be176cdcead06a8cdcca9eb9
benjamin.hlina <benjamin.hlina@gmail.com>
2023-05-24 17:14:43 -0400

    Changed names of model fit ids and added in plot and regression for temp vs. isotope raster

--- R/acoustic data with isotopes/isotope_mean_temp.R
+++ R/acoustic data with isotopes/isotope_mean_temp.R
@@ -122,7 +122,7 @@ ggsave(filename = here("Plots",
 write_rds(p, here("Saved Plots",
                   "c_13_temp_overall.rds"))
 
-# ---- nitrogren vs. temp -----
+# ---- nitrogren vs. temp plot -----
 ggplot(data = ati, aes(y = n_15, x = mean_temp)) + 
   geom_point(size = 4, aes(fill = fish_basin), 
              shape = 21, stroke = 0.8) + 
@@ -370,9 +370,9 @@ me_d15n <- tidy(car::Anova(m7, type = "III"))
 # ---- combine main effects and model fit together and save as an RDS ----
 # we will combine the rds for all three metrics later in another script 
 
-model_fit <- bind_rows(list(gs_d13c = gs_d13c, 
-                            gs_d15n = gs_d15n,
-                            gs_d15n_outlier = gs_d15n_outlier), 
+model_fit <- bind_rows(list(d13c = gs_d13c, 
+                            d15n = gs_d15n,
+                            d15n_outlier = gs_d15n_outlier), 
                        .id = "id") %>% 
   mutate(
     metric = "temp"
@@ -380,9 +380,9 @@ model_fit <- bind_rows(list(gs_d13c = gs_d13c,
   dplyr::select(id, metric, model, r.squared:df.residual)
 
 
-model_effects <- bind_rows(list(me_d13c = me_d13c, 
-                                me_d15n = me_d15n,
-                                me_d15n_outlier = me_d15n_outlier), 
+model_effects <- bind_rows(list(d13c = me_d13c,
+                                d15n = me_d15n,
+                                d15n_outlier = me_d15n_outlier), 
                            .id = "id") %>% 
   mutate(
     metric = "temp"
@@ -399,105 +399,93 @@ write_rds(model_effects, here("Results",
                               "temp_isotope_model_effects.rds"))
 
 # ---- plot mean temp across months for d13C -----
-# descdist(ati$mean_temp)
-# 
-# 
-# m7 <- glm(mean_temp ~ c_13 * n_15, 
-#           data = ati, 
-#           family = Gamma()
-#           )
-# 
-# Anova(m7, type = 3)
-# 
-# res <- simulateResiduals(m7)
-# 
-# plot(res)
-# shapiro.test(residuals.glm(m7))
-# 
-# 
-# 
-# summary(ati$n_15)
-# # 
-# nd <- expand_grid(
-#   c_13 = seq(-25.5, -31.5, length.out = 100),
-#   n_15 = seq(8.75, 12, length.out = 100),
-#   mean_temp = seq(4.25, 7.75, length.out = 100)
-# 
-# )
-# # 
-# fits <- predict(m7, newdata = nd, type = "response")
-# 
-# preds <- bind_cols(nd, fits) %>%
-#   rename(
-#     fit = ...4
-#   )
-# 
-# preds
-# # 
-# # 
-# # 
-# # 
-# # 
-# # 
-# # 
-# ggplot() +
-#  
-#   geom_raster(data = preds, aes(x = c_13,
-#                               y = n_15, fill = fit), ) + 
-#   geom_point(data = ati, size = 4,
-#              aes(y = n_15, x = c_13,
-#                  colour = mean_temp),
-#              # shape = 21, stroke = 0.8
-#              ) +
-# #   # stat_ellipse(aes(colour = fish_basin), 
-# #   #              type = "norm", linetype = 1,
-# #   # linewidth = 1) +
-# #   # geom_errorbar(aes(xmin = mean_temp - sem, 
-# #   #                   xmax = mean_temp + sem), width = 0.05) +
-#   scale_fill_viridis_c(name = "Predicted\nTemperature Use (°C)", 
-#                        option = "D",alpha = 0.5, 
-#                        breaks = seq(4.5, 7.5, 1),
-#                        # limit = c(4, 8)
-#                        ) +
-#   scale_colour_viridis_c(
-#     # begin = 0.25, end = 0.85,
-#                        option = "D",
-#                        name = "Observed\nTemperature Use (°C)", 
-#                        ) +
-#   # scale_y_continuous(breaks = rev(seq(-26, -31, -1))) +
-#   # facet_wrap(.~ fish_basin) +
-#   coord_cartesian(expand = FALSE) +
-#   theme_bw(base_size = 15) +
-#   theme(
-#     legend.title = element_text(hjust = 0.5), 
-#     panel.grid = element_blank(),
-#     # legend.position = c(0.85, 0.9)
-#   ) +
-#   labs(
-#     x = expression(paste(delta ^ 13, "C")),
-#     y = expression(paste(delta ^ 15, "N"))) -> p3
-# 
-# 
-# p3
-# ggsave(filename = here("Plots",
-#                        "temp use and isotopes",
-#                        "mean_temp_month_d13c_raster_pred.png"), plot = p3,
-#        width = 11, height = 8.5)
-# # 
-# 
-# write_rds(p, here("Saved Plots",
-#                   "c_13_temp_overall.rds"))
-# 
-# 
-# ggplot(data = preds, aes(x = mean_temp, y = fit)) + 
-#   geom_(aes(fill = fit), linewidth = 1) + 
-#   scale_fill_viridis_c(option = "D", 
-#                        name = expression(paste(delta ^ 13, "C"))) + 
-#   theme_bw(base_size = 15) + 
-#   facet_wrap(.~ fish_basin) +
-#   theme(
-#     panel.grid = element_blank()
-#   ) + 
-#   labs(
-#     y = expression(paste(delta ^ 13, "C")), 
-#     x = "Mean Monthly Temperature Use (°C)"
+descdist(ati$mean_temp)
+
+
+m7 <- glm(mean_temp ~ c_13 * n_15,
+          data = ati,
+          family = Gamma()
+          )
+
+Anova(m7, type = 3)
+
+res <- simulateResiduals(m7)
+
+plot(res)
+shapiro.test(residuals.glm(m7))
+
+
+
+summary(ati$mean_temp)
+#
+nd <- expand_grid(
+  c_13 = seq(-25.5, -31.5, length.out = 100),
+  n_15 = seq(8.5, 15, length.out = 100),
+  mean_temp = seq(4, 8, length.out = 100)
+
+)
+#
+fits <- predict(m7, newdata = nd, type = "response")
+
+preds <- bind_cols(nd, fits) %>%
+  rename(
+    fit = ...4
+  )
+
+preds
+#
+#
+#
+#
+#
+#
+#
+ggplot() +
+
+  geom_raster(data = preds, aes(x = c_13,
+                              y = n_15, fill = fit), ) +
+  geom_point(data = ati, size = 4,
+             aes(y = n_15, x = c_13,
+                 fill = mean_temp),
+             shape = 21, stroke = 0.8
+             ) +
+#   # stat_ellipse(aes(colour = fish_basin),
+#   #              type = "norm", linetype = 1,
+#   # linewidth = 1) +
+#   # geom_errorbar(aes(xmin = mean_temp - sem,
+#   #                   xmax = mean_temp + sem), width = 0.05) +
+  scale_fill_viridis_c(name = "Temperature Use (°C)",
+                       option = "D",alpha = 0.5,
+                       breaks = seq(4, 8, 1),
+                       limit = c(4, 8)
+                       ) +
+  # scale_colour_viridis_c(
+  #   # begin = 0.25, end = 0.85,
+  #                      option = "D",
+  #                      name = "Observed\nTemperature Use (°C)",
+  #                      ) +
+  scale_x_continuous(breaks = rev(seq(-25, -31, -1))) + 
+  scale_y_continuous(breaks = seq(8, 15, 1)) + 
+  # scale_colour_viridis_c(
+  # scale_y_continuous(breaks = rev(seq(-26, -31, -1))) +
+  # facet_wrap(.~ fish_basin) +
+  coord_cartesian(expand = FALSE) +
+  theme_bw(base_size = 15) +
+  theme(
+    legend.title = element_text(hjust = 0.5),
+    panel.grid = element_blank(),
+    # legend.position = c(0.85, 0.9)
+  ) +
+  labs(
+    x = expression(paste(delta ^ 13, "C")),
+    y = expression(paste(delta ^ 15, "N"))) -> p3
+
+
+p3
+ggsave(filename = here("Plots",
+                       "temp use and isotopes",
+                       "mean_temp_month_d13c_raster_pred.png"), plot = p3,
+       width = 11, height = 8.5)
+
+write_rds(p3, here("Saved Plots",
+                   "d13c_d15n_temp_predicted.rds"))
